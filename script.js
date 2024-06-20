2f9eb3b4fa6481c9fb0c872a442602ec8b081463
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-06-20 21:50:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function toggleAnswer(question) {
+    var answer = question.nextElementSibling;
+    if (answer.style.display === "none") {
+      answer.style.display = "block";
+      question.querySelector(".plus").innerHTML = "-";
+    } else {
+      answer.style.display = "none";
+      question.querySelector(".plus").innerHTML = "+";
+    }
+  }
